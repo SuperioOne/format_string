b725a07463f5bdd80f346c740775496eb627657f
Timur Olur <timurolur@gmail.com>
2023-12-13 12:13:45 +0300

    added benchmark against some popular packages

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # Yet another format string function
 
-## Project Description
+Fast, small and basic string templating function. This library is intended for use cases where the position of parameters 
+in a format string is not known at compile-time.
 
-This library is intended for use cases where the position of parameters in a format string is not known at compile-time.
-It is particularly useful for handling parametric localization texts, where the text itself may contain placeholders
-for dynamic values.
-
-> **For situations with predictable string formats, using JavaScript's built-in string templating is highly recommended.**
+> **For situations with predictable/constant string formats, using JavaScript's built-in string templating is highly recommended.**
 
 ## Getting Started
 
@@ -95,6 +92,47 @@ npm run build
 ## Contributing
 
 Pull requests are welcome, especially ideas for time and memory allocation reduction.
+
+## Benchmarks
+Tested against some popular packages with four parameters and single escape.
+
+> Keep in mind some of the listed libraries provides more features like number formatting, padding etc.
+
+```
+Test Environment:
+CPU       : AMD Ryzen 9 5950X 16-Core @ 32x 5.177GHz
+OS        : x86_64 Linux 6.6.3-1
+node      : v20.7.0
+vitest    : v1.0.4
+tinybench : v2.5.1
+
+name                                                        hz     min     max    mean     p75     p99    p995    p999     rme  samples
+· @superior-one/format_string with positional args  2,667,454.58  0.0003  0.8361  0.0004  0.0004  0.0004  0.0006  0.0016  ±0.30%  2667455   fastest
+· @superior-one/format_string with named args       2,202,596.97  0.0004  0.6137  0.0005  0.0005  0.0005  0.0005  0.0009  ±0.21%  2202597  
+· string-template with named args                     866,472.63  0.0011  0.2794  0.0012  0.0012  0.0012  0.0013  0.0018  ±0.16%   866473  
+· string-template with positional args                993,227.21  0.0009  1.0439  0.0010  0.0010  0.0011  0.0012  0.0018  ±0.24%   993228  
+· @stdlib/string-format with positional args        1,740,323.68  0.0005  0.1582  0.0006  0.0006  0.0007  0.0008  0.0010  ±0.18%  1740324  
+· string-format with positional args                  534,816.99  0.0017  0.2324  0.0019  0.0018  0.0037  0.0038  0.0051  ±0.22%   534817  
+· string-format with named args                       370,986.79  0.0025  0.1357  0.0027  0.0027  0.0029  0.0030  0.0061  ±0.15%   370987   slowest
+· pupa with positional args                           512,409.23  0.0018  0.1490  0.0020  0.0019  0.0021  0.0022  0.0039  ±0.14%   512410  
+· pupa with named args                                455,322.46  0.0021  0.1525  0.0022  0.0022  0.0023  0.0024  0.0045  ±0.14%   455323  
+· string replaceAll positional args function        1,604,997.66  0.0006  0.1082  0.0006  0.0006  0.0007  0.0008  0.0012  ±0.12%  1604998  
+· string replaceAll named args function             1,709,737.81  0.0005  0.7950  0.0006  0.0006  0.0006  0.0008  0.0012  ±0.17%  1709739  
+
+BENCH  Summary
+
+@superior-one/format_string with positional args - index.bench.js >
+1.21x faster than @superior-one/format_string with named args
+1.53x faster than @stdlib/string-format with positional args
+1.56x faster than string replaceAll named args function
+1.66x faster than string replaceAll array args function
+2.69x faster than string-template with positional args
+3.08x faster than string-template with named args
+4.99x faster than string-format with positional args
+5.21x faster than pupa with positional args
+5.86x faster than pupa with named args
+7.19x faster than string-format with named args
+```
 
 ## License
 
---
+++ benchmarks/index.bench.js
@@ -0,0 +1,90 @@
+import {bench} from 'vitest';
+import string_template from "string-template";
+import string_format from "string-format";
+import stdlib_string_format from "@stdlib/string-format";
+import pupa from 'pupa';
+import format_string from "../index";
+
+const positionalArgs = [0, 1, 2, 3];
+const namedArgs = {
+  "alpha": "A",
+  "beta": "B",
+  "charlie": "C",
+  "delta": "D"
+};
+
+console.log("format_string:", format_string("{0}{1}{2}{3} {{3}}", positionalArgs));
+console.log("format_string:", format_string("{charlie}{delta}{alpha}{beta} {{delta}}", namedArgs));
+console.log("string_template:", string_template("{charlie}{delta}{alpha}{beta} {{delta}}", namedArgs));
+console.log("string_template:", string_template("{0}{1}{2}{3} {{3}}", positionalArgs));
+console.log("stdlib_string_format:", stdlib_string_format("%s%s%s%s {3}", 0, 1, 2, 3));
+console.log("string_format:", string_format("{0}{1}{2}{3} {{3}}", 0, 1, 2, 3));
+console.log("string_format:", string_format("{charlie}{delta}{alpha}{beta} {{delta}}", namedArgs));
+console.log("pupa:", pupa("{0}{1}{2}{3} {{3}}", positionalArgs));
+console.log("pupa:", pupa("{charlie}{delta}{alpha}{beta} {{delta}}", namedArgs));
+
+bench('@superior-one/format_string with positional args', () => {
+  return format_string("{0}{1}{2}{3} {{3}}", positionalArgs);
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+bench('@superior-one/format_string with named args', () => {
+  return format_string("{charlie}{delta}{alpha}{beta} {{delta}}", namedArgs);
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+bench('string-template with named args', () => {
+  return string_template("{charlie}{delta}{alpha}{beta} {{delta}}", namedArgs);
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+bench('string-template with positional args', () => {
+  return string_template("{0}{1}{2}{3} {{3}}", positionalArgs);
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+bench('@stdlib/string-format with positional args', () => {
+  return stdlib_string_format("%s%s%s%s {3}", 0, 1, 2, 3);
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+bench('string-format with positional args', () => {
+  return string_format("{0}{1}{2}{3} {{3}}", 0, 1, 2, 3);
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+bench('string-format with named args', () => {
+  return string_format("{charlie}{delta}{alpha}{beta} {{delta}}", namedArgs);
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+bench('pupa with positional args', () => {
+  return pupa("{0}{1}{2}{3} {{3}}", positionalArgs);
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+bench('pupa with named args', () => {
+  return pupa("{charlie}{delta}{alpha}{beta} {{delta}}", namedArgs);
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+// replaceAll does not escape but added anyway.
+bench('string replaceAll positional args function', () => {
+  let result = "{0}{1}{2}{3} {{3}}";
+
+  for (let i = 0; i < positionalArgs.length; i++) {
+    result = result.replaceAll(`{${i}}`, positionalArgs[i]);
+  }
+  return result;
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+bench('string replaceAll named args function', () => {
+  let result = "{alpha}{beta}{charlie}{delta} {{delta}}";
+  for (const key in namedArgs) {
+    result = result.replaceAll(`{${key}}`, namedArgs[key]);
+  }
+  return result;
+}, {time: 1000, warmupIterations: 10, iterations: 100})
+
+// https://stackoverflow.com/a/61634647
+bench("stackoverflow named args", () => {
+
+  return "{alpha}{beta}{charlie}{delta} {{delta}}".replace(
+    /{(\w+)}/g,
+    (placeholderWithDelimiters, placeholderWithoutDelimiters) =>
+      namedArgs.hasOwnProperty(placeholderWithoutDelimiters) ?
+        namedArgs[placeholderWithoutDelimiters] : placeholderWithDelimiters
+  );
+
+}, {time: 1000, warmupIterations: 10, iterations: 100})
